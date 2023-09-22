9f21a02ba451d48a9f98f39e6b35e8e14209c2c8
Paul5175 <92270605+Paul5175@users.noreply.github.com>
2023-09-22 23:59:05 +0200

    Update test.py

--- component/test.py
+++ component/test.py
@@ -1,17 +1,15 @@
 import pytest
 import bip39
 
-# Test fonctionnel
-def test_functional_create_and_convert():
-    # Générer une clé privée aléatoire
-    private_key = "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+# Générer une clé privée aléatoire
+private_key = "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
 
-    # Générer une suite mnémonique depuis la clé privée
-    mnemonic_phrase = bip39.create_mnemonic_from_private_key(private_key, "en")
+# Générer une suite mnémonique depuis la clé privée
+mnemonic_phrase = bip39.create_mnemonic_from_private_key(private_key, "en")
 
-    # Convertir la suite mnémonique en clé privée
-    converted_private_key = bip39.convert_to_private_key(mnemonic_phrase, "en")
+# Convertir la suite mnémonique en clé privée
+converted_private_key = bip39.convert_to_private_key(mnemonic_phrase, "en")
 
-    # Vérifier que la clé privée convertie est égale à l'originale
-    assert private_key == converted_private_key
+# Vérifier que la clé privée convertie est égale à l'originale
+assert private_key == converted_private_key
 
