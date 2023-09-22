a6abdaf0fe2d065e0caf94a8fefd112f05b04719
Paul5175 <92270605+Paul5175@users.noreply.github.com>
2023-09-22 15:00:19 +0200

    Update test.py

--- component/test.py
+++ component/test.py
@@ -1,3 +1,16 @@
-import crypto_component
-print("version: " + crypto_component.getVersion())
-print("message du composant: " + crypto_component.greet())
+#include <gtest/gtest.h>
+#include "bip39.cpp"
+
+TEST(Bip39Test, CreateAndValidateRecoveryPhrase) {
+    // Test creating and validating a recovery phrase
+    std::string recovery_phrase = create_recovery_phrase(24, "en");
+    EXPECT_TRUE(validate_recovery_phrase(recovery_phrase, "en"));
+}
+
+// Add more test cases as needed
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
