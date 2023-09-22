a13f42c381b4f6978aaf39e76640dc95d7dcea81
Paul5175 <92270605+Paul5175@users.noreply.github.com>
2023-09-22 16:15:32 +0200

    Update test.py

--- component/test.py
+++ component/test.py
@@ -1,16 +1,36 @@
-#include <gtest/gtest.h>
-#include "bip39.cpp"
+import pytest
+import bip39
 
-TEST(Bip39Test, CreateAndValidateRecoveryPhrase) {
-    // Test creating and validating a recovery phrase
-    std::string recovery_phrase = create_recovery_phrase(24, "en");
-    EXPECT_TRUE(validate_recovery_phrase(recovery_phrase, "en"));
-}
+def test_create_recovery_phrase():
+    # Test avec un nombre de mots valide
+    try:
+        bip39.create_recovery_phrase(12, "en")
+    except Exception:
+        pytest.fail("Expected no exception for valid word count")
 
-// Add more test cases as needed
+    # Test avec un nombre de mots en dehors de l'intervalle [12, 15, 18, 21, 24]
+    with pytest.raises(ValueError):
+        bip39.create_recovery_phrase(10, "en")
 
-int main(int argc, char** argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
+def test_validate_recovery_phrase():
+    # Test avec une phrase mnémonique vide
+    with pytest.raises(ValueError):
+        bip39.validate_recovery_phrase("", "en")
+
+    # Test avec une phrase mnémonique valide
+    recovery_phrase = bip39.create_recovery_phrase(15, "en")
+    assert bip39.validate_recovery_phrase(recovery_phrase, "en") == True
+
+    # Test avec une phrase mnémonique invalide
+    invalid_phrase = "invalid phrase here"
+    assert bip39.validate_recovery_phrase(invalid_phrase, "en") == False
+
+def test_extract_entropy():
+    # Test de l'extraction de l'entropie d'une phrase de récupération
+    recovery_phrase = bip39.create_recovery_phrase(18, "en")
+    entropy = bip39.extract_entropy(recovery_phrase, "en")
+    assert len(entropy) == 64  # 64 caractères hexadécimaux
+
+if __name__ == "__main__":
+    pytest.main()
 
